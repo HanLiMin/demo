d0a8b6dd0f6aa1d750acf06491d0289607137ff4
han <hanlimin.code@foxmail.com>
2017-12-27 17:39:46 +0800

    公司修改

--- src/main/java/com/moontwon/demo/nio/simple/Client.java
+++ src/main/java/com/moontwon/demo/nio/simple/Client.java
@@ -7,6 +7,10 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author hanlimin
@@ -14,38 +18,45 @@ import java.util.Iterator;
  * @date 17-12-26
  */
 public class Client {
-    public static void main(String[] args) throws IOException {
-        SocketChannel socketChannel = SocketChannel.open();
-        Selector selector = Selector.open();
-        socketChannel.configureBlocking(false);
-        socketChannel.connect(new InetSocketAddress("127.0.0.1",6666));
-        socketChannel.register(selector, SelectionKey.OP_CONNECT);
-        for(;;){
-            if (selector.select()>0){
-                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-                while (iterator.hasNext()){
-                    SelectionKey key = iterator.next();
-                    iterator.remove();
-                    if (key.isConnectable()){
-                        socketChannel.finishConnect();
-                        socketChannel.register(selector,SelectionKey.OP_WRITE);
-                        System.err.println("client side finish connet");
-                    }else if(key.isWritable()){
-                        System.err.println("client side writable");
-                        socketChannel.write(ByteBuffer.wrap("6666".getBytes()));
-                        socketChannel.register(selector,SelectionKey.OP_WRITE);
-                    }else if(key.isReadable()){
-                        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
-                        socketChannel.read(byteBuffer);
-                        byteBuffer.flip();
-                        byte[] bytes = new byte[byteBuffer.remaining()];
-                        byteBuffer.get(bytes);
-                        System.err.println(new String(bytes));
-                        socketChannel.close();
-                        return;
-                    }
-                }
-            }
-        }
-    }
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(Client.class);
+	public static void main(String[] args) throws IOException {
+		SocketChannel socketChannel = SocketChannel.open();
+		Selector selector = Selector.open();
+		socketChannel.configureBlocking(false);
+		socketChannel.connect(new InetSocketAddress("127.0.0.1", 6666));
+		socketChannel.register(selector, SelectionKey.OP_CONNECT);
+		Scanner scanner = new Scanner(System.in);
+		boolean stop = false;
+		while (!stop) {
+			if (selector.select() > 0) {
+				Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+				while (iterator.hasNext()) {
+					SelectionKey key = iterator.next();
+					iterator.remove();
+					if (key.isConnectable()) {
+						socketChannel.finishConnect();
+						socketChannel.register(selector, SelectionKey.OP_WRITE | SelectionKey.OP_READ);
+						LOGGER.info("client side finish connet");
+					}
+					if (key.isWritable()) {
+						LOGGER.info("发送消息：");
+						socketChannel.write(ByteBuffer.wrap(scanner.nextLine().getBytes()));
+					}
+					if (key.isReadable()) {
+						scanner.close();
+						LOGGER.info("===================================");
+						ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+						socketChannel.read(byteBuffer);
+						byteBuffer.flip();
+						byte[] bytes = new byte[byteBuffer.remaining()];
+						byteBuffer.get(bytes);
+						LOGGER.info("收到消息 {}", new String(bytes));
+						socketChannel.close();
+						break;
+					}
+				}
+			}
+		}
+	}
 }
--- src/main/java/com/moontwon/demo/nio/simple/Server.java
+++ src/main/java/com/moontwon/demo/nio/simple/Server.java
@@ -1,7 +1,5 @@
 package com.moontwon.demo.nio.simple;
 
-import sun.applet.Main;
-
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -11,75 +9,116 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author hanlimin
  * @email hanlimin.code@foxmail.com
  * @date 17-12-26
  */
 public class Server extends Thread {
-    public static void main(String[] args) throws IOException {
-        new Server().start();
-    }
 
-    private ServerSocketChannel serverSocketChannel;
-    private Selector selector;
+	private final static Logger LOGGER = LoggerFactory.getLogger(Server.class);
+	public static void main(String[] args) throws IOException {
+		new Server().start();
+	}
 
-    public Server() throws IOException {
-        serverSocketChannel = ServerSocketChannel.open();
-        serverSocketChannel.configureBlocking(false);
-        serverSocketChannel.bind(new InetSocketAddress("127.0.0.1", 6666));
-        selector = Selector.open();
-        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-    }
+	private ServerSocketChannel serverSocketChannel;
+	private Selector selector;
 
-    @Override
-    public void run() {
-        for (; ; ) {
-            try {
-                if (selector.select() > 0) {
-                    Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-                    while (iterator.hasNext()) {
-                        SelectionKey key = iterator.next();
-                        iterator.remove();
-                        if (key.isAcceptable()) {
-                            System.err.println("server accept");
-                            SocketChannel socketChannel = serverSocketChannel.accept();
-                            socketChannel.configureBlocking(false);
-                            socketChannel.register(selector, SelectionKey.OP_READ);
-                        } else if (key.isReadable()) {
-                            System.err.println("server side client be readable");
-                            ByteBuffer byteBuffer = ByteBuffer.allocate(512);
-                            Boolean r = (Boolean) key.attachment();
-                            SocketChannel channel = (SocketChannel) key.channel();
-                            if (r != null && r == Boolean.TRUE) {
-                                channel.read(byteBuffer);
-                                byteBuffer.flip();
-                                byte[] bytes = new byte[byteBuffer.remaining()];
-                                byteBuffer.get(bytes);
-                                System.err.println(new String(bytes));
-                                channel.close();
-                                System.err.println("server side client close");
-                            }else {
-                                channel.read(byteBuffer);
-                                byteBuffer.flip();
-                                byte[] bytes = new byte[byteBuffer.remaining()];
-                                byteBuffer.get(bytes);
-                                System.err.println(new String(bytes));
-                                channel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE, true);
-                            }
+	public Server() throws IOException {
+		serverSocketChannel = ServerSocketChannel.open();
+		serverSocketChannel.configureBlocking(false);
+		serverSocketChannel.bind(new InetSocketAddress("127.0.0.1", 6666));
+		selector = Selector.open();
+		serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+	}
 
-                        } else if (key.isWritable()) {
-                            SocketChannel channel = (SocketChannel) key.channel();
-                            channel.write(ByteBuffer.wrap("server said:byte".getBytes()));
-                            System.err.println("server side client be writable");
-                        }
+	@Override
+	public void run() {
+		for (;;) {
+			try {
+				if (selector.select() > 0) {
+					Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+					while (iterator.hasNext()) {
+						SelectionKey key = iterator.next();
+						iterator.remove();
+						LOGGER.info("{}",key.hashCode());
+						if (key.isValid()) {
+							if (key.isAcceptable())
+								accept(key);
+							else if (key.isReadable())
+								read(key);
+							else if (key.isWritable())
+								write(key);
+						}else {
+							LOGGER.info("key not valid");
+						}
+						
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+		}
+	}
+	private void accept(SelectionKey key) {
+		try {
+			SocketChannel socketChannel = serverSocketChannel.accept();
+			socketChannel.configureBlocking(false);
+			socketChannel.register(selector, SelectionKey.OP_READ);
+		} catch (Exception e) {
+			handleException("接收客户端连接出现异常", e);
+			key.cancel();
+		}
 
-        }
-    }
+	}
+	private void read(SelectionKey key) {
+		if (key.isValid()) {
+			ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+			SocketChannel channel = (SocketChannel) key.channel();
+			try {
+				channel.read(byteBuffer);
+				byteBuffer.flip();
+				byte[] bytes = new byte[byteBuffer.remaining()];
+				byteBuffer.get(bytes);
+				String msg = new String(bytes);
+				if ("bye".equals(msg)) {
+					channel.register(selector, SelectionKey.OP_WRITE);
+					LOGGER.info("收到 bye");
+				}
+				LOGGER.info("服务端收到客户端消息: {}", msg);
+			} catch (Exception e) {
+				handleException("接收客户端消息读取", e);
+				key.cancel();
+			}
+		}else {
+			LOGGER.info("有问题");
+			key.cancel();
+		}
+		
+
+	}
+
+	private void write(SelectionKey key) {
+		SocketChannel channel = (SocketChannel) key.channel();
+		try {
+			channel.write(ByteBuffer.wrap("bye".getBytes()));
+			LOGGER.info("服务端已发送消息");
+			key.interestOps(SelectionKey.OP_READ);
+		} catch (Exception e) {
+			handleException("发送消息发生异常", e);
+			key.cancel();
+		}
+
+	}
+
+	private void handleException(String msg, Exception e) {
+		LOGGER.info("{}", msg, e);
+	}
+
+	class Client {
+	}
 }
